502c0af0d33d662aec3ee85ddaa178ac27a03771
Hafeed10 <hafeex@gamil.com>
2024-07-13 18:09:43 +0530

    slide section in comlited

---
+++ src/Data/image.js
@@ -0,0 +1,23 @@
+import img1 from '../components/asetss/Home/images.jpeg'
+import img2 from '../components/asetss/Home/img12.jpeg'
+import img3 from '../components/asetss/Home/img3.jpeg'
+import img4 from '../components/asetss/Home/img4.jpeg'
+import img5 from '../components/asetss/Home/img5.jpeg'
+import img6 from '../components/asetss/Home/img13.jpeg'
+import img11 from '../components/asetss/Home/img3.jpeg'
+import img8 from '../components/asetss/Home/img15.jpeg'
+import img12 from '../components/asetss/Home/img12.jpeg'
+
+
+export const dtaimg = [
+img1,
+img2,
+img3,
+img4,
+img5,
+img11,
+img8,
+img12,
+img6
+]
+export default dtaimg;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import dtaimg from '../../Data/image';
 
 const Slider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = dtaimg; // Use the imported images array
+
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 1000); // Change slide every 5 seconds
+
+    return () => clearInterval(slideInterval);
+  }, [images.length]);
+
   return (
-    <div>
-      
+    <div className="flex flex-col md:flex-row justify-between ml-10 py-10 px-4 md:px-12">
+      <div>
+        <div className="font-bold">
+          <h1 className="text-2xl line-through">New Movies Slider</h1>
+        </div>
+        <div className="relative overflow-hidden py-10">
+          <div
+            className="flex transition-transform duration-700"
+            style={{ transform: `translateX(-${currentIndex * 10}%)`, width: `${images.length * 10}%` }}
+          >
+            {images.map((src, index) => (
+              <div key={index} className="flex-shrink-0 w-full md:w-1/5 px-1">
+                <img src={src} alt={`Slide ${index + 1}`} className="block mx-auto object-contain" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Slider;
