7f2ef651b5656a05e2f0b7b1c5ef897ab74e6419
Hafeed10 <hafeex@gamil.com>
2024-07-13 09:09:49 +0530

    navbar finish

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -6,53 +6,50 @@ import { IoIosContact } from "react-icons/io";
 import { CiLogin } from "react-icons/ci";
 import { GoSignIn } from "react-icons/go";
 
-
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <div className="flex w-fit space-x-6 py-10 px-12">
-      <div className='flex items-center justify-between border-b md:w-full w-full space-x-10'>
-      <div className='flex items-center  '>
-      <img src={Logo} alt="" className="w-10 h-auto" />
-      <h1 className='font-bold ml-3 md:text-2xl text-3xl '>AVESHAM</h1>
-      </div> 
-      <ul className="flex justify-center cursor-pointer hidden xl:flex">
-        <li className="mr-6">Home</li>
-        <li className="mr-6">About</li>
-        <li className="mr-6">Services</li>
-        <li>Blog</li>
-      </ul>
-      <div className="flex justify-end items-center">
-        <div className="font-bold py-2 px-4 rounded">
-          <CiLogin color='black' size={30} />
-        </div>
-        <div  className=" text-white font-bold py-2 px-4 rounded ml-2">
-        <GoSignIn size={30} color='black'/>
-        </div>
-        <div className=" font-bold py-2 px-4 rounded ml-2">
-          <IoIosContact size={30} color='black' />
-        </div>
-        <div className='xl:hidden md:block mr-20' onClick={toggleMenu}>
-          <RiMenuFold3Fill size={30} color='black' />
-        </div>
-     
-        {/* // ! Responsive Navbar setting start  */}
-        {isOpen && (
-          <div className="absolute top-0 w-1/2 bg-white shadow-md py-4 flex flex-col items-center">
-            <IoMdClose size={30} className="mb-4 cursor-pointer  ml-20" onClick={toggleMenu} />
-            <ul className='cursor-pointer '>
-              <li className="px-4 py-2">Home</li>
-              <li className="px-4 py-2">About</li>
-              <li className="px-4 py-2">Services</li>
-              <li className="px-4 py-2">Blog</li>
-            </ul>
+    <div className="max flex items-center justify-between border-b  w-full  space-x-6 py-10 px-12">
+        <div className='flex items-center  '>
+          <img src={Logo} alt="" className="w-10 h-auto" />
+          <h1 className='font-bold ml-3 md:text-2xl text-3xl '>AVESHAM</h1>
+        </div> 
+        <ul className="flex justify-center cursor-pointer hidden xl:flex">
+          <li className="mr-6">Home</li>
+          <li className="mr-6">About</li>
+          <li className="mr-6">Services</li>
+          <li>Blog</li>
+        </ul>
+        <div className="flex justify-end items-center">
+          <div className="font-bold py-2 px-4 rounded">
+            <CiLogin color='black' size={30} />
           </div>
-        )}
-          {/* // ! Responsive Navbar setting end  */}
-      </div>
-      </div>
+          <div  className=" text-white font-bold py-2 px-4 rounded ml-2">
+            <GoSignIn size={30} color='black'/>
+          </div>
+          <div className=" font-bold py-2 px-4 rounded ml-2">
+            <IoIosContact size={30} color='black' />
+          </div>
+          <div className='xl:hidden md:block mr-20' onClick={toggleMenu}>
+            <RiMenuFold3Fill size={30} color='black' />
+          </div>
+        
+          {/* // ! Responsive Navbar setting start  */}
+          {isOpen && (
+            <div className="absolute top-0 w-1/2 bg-white shadow-md py-4 flex flex-col items-center">
+              <IoMdClose size={30} className="mb-4 cursor-pointer  ml-20" onClick={toggleMenu} />
+              <ul className='cursor-pointer '>
+                <li className="px-4 py-2">Home</li>
+                <li className="px-4 py-2">About</li>
+                <li className="px-4 py-2">Services</li>
+                <li className="px-4 py-2">Blog</li>
+              </ul>
+            </div>
+          )}
+            {/* // ! Responsive Navbar setting end  */}
+        </div>
     </div>
   );
 }
